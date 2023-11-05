b7fb0935db001615fff2014a0a32cc8121e02b26
Alexandria P <alxpettit@gmail.com>
2023-11-05 09:16:36 -0800

    finally everything works

--- flake.nix
+++ flake.nix
@@ -1,34 +1,157 @@
 {
+  description = "Anayzer for WGSL shader language";
+
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+
     wgsl-analyzer = {
       url = "github:wgsl-analyzer/wgsl-analyzer";
       flake = false;
     };
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs, wgsl-analyzer }:
+  outputs = { self, nixpkgs, crane, fenix, flake-utils, advisory-db, wgsl-analyzer, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = (import nixpkgs) {
+        pkgs = import nixpkgs {
           inherit system;
         };
 
-        naersk' = pkgs.callPackage naersk {};
-      in {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
+        inherit (pkgs) lib;
+
+        craneLib = crane.lib.${system};
+        src = wgsl-analyzer;
+        # src = craneLib.cleanCargoSource wgsl-analyzer;
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          buildInputs = [
+            # Add additional build inputs here
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+        };
+
+        craneLibLLvmTools = craneLib.overrideToolchain
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "llvm-tools"
+            "rustc"
+          ]);
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        my-crate = craneLib.buildPackage (commonArgs // {
           pname = "wgsl-analyzer";
-          src = wgsl-analyzer;
+          doCheck = false;
+          installPhaseCommand = ''
+            mkdir -p $out $out/bin
+            cp target/release/wgsl_analyzer $out/bin/wgsl_analyzer
+            ln -s $out/bin/wgsl_analyzer $out/bin/wgsl-analyzer
+          '';
+          # version = craneLib.crateNameFromCargoToml { cargoToml = ./path/to/Cargo.toml; };
+          # everything in wgsl-analyzer is fixed at 0.0.0 except human text
+          # so IDK what to do here
+          version = "0.0.0";
+          inherit cargoArtifacts;
+        });
+      in
+      {
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit my-crate;
+
+          # # Run clippy (and deny all warnings) on the crate source,
+          # # again, resuing the dependency artifacts from above.
+          # #
+          # # Note that this is done as a separate derivation so that
+          # # we can block the CI if there are issues here, but not
+          # # prevent downstream consumers from building our crate by itself.
+          # my-crate-clippy = craneLib.cargoClippy (commonArgs // {
+          #   inherit cargoArtifacts;
+          #   cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          # });
+
+          # my-crate-doc = craneLib.cargoDoc (commonArgs // {
+          #   inherit cargoArtifacts;
+          # });
+
+          # # Check formatting
+          # my-crate-fmt = craneLib.cargoFmt {
+          #   inherit src;
+          # };
+
+          # # Audit dependencies
+          # my-crate-audit = craneLib.cargoAudit {
+          #   inherit src advisory-db;
+          # };
+
+          # # Audit licenses
+          # my-crate-deny = craneLib.cargoDeny {
+          #   inherit src;
+          # };
+
+          # # Run tests with cargo-nextest
+          # # Consider setting `doCheck = false` on `my-crate` if you do not want
+          # # the tests to run twice
+          # my-crate-nextest = craneLib.cargoNextest (commonArgs // {
+          #   inherit cargoArtifacts;
+          #   partitions = 1;
+          #   partitionType = "count";
+          # });
         };
 
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
+        packages = {
+          default = my-crate;
+          wgsl-nalyzer = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
+            inherit cargoArtifacts;
+          });
         };
-      }
-    );
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = my-crate;
+        };
+
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [
+            # pkgs.ripgrep
+          ];
+        };
+      });
 }
-
